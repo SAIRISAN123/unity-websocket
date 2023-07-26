4b8cf126b9e8ec056c1a28edc901fab926ba323c
Mike Schweitzer <mschweitzer@gmail.com>
2023-07-26 10:48:57 -0700

    Fix compile error in WebGL builds

--- Runtime/Internal/DotNetWebSocket.cs
+++ Runtime/Internal/DotNetWebSocket.cs
@@ -1,3 +1,4 @@
+#if !UNITY_WEBGL || UNITY_EDITOR
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -169,3 +170,4 @@ namespace MikeSchweitzer.WebSocket.Internal
         #endregion
     }
 }
+#endif
